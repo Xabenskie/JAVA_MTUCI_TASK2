4f4351e5c02f135b8a80abb4df90a9c3ec008ffc
Kostya <lukyanovKR@gmail.com>
2024-09-25 16:13:46 +0300

    [+] Таски 1-10

--- Task1-10.java
+++ Task1-10.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 class Task1_10 {
 	static public void main(String[] args) {
-		int[] numbers = {3, 12, 7, 81, 52};
-		System.out.println(getInitials("simonov sergei evgenievich"));
+		int[] numbers = {3, 5, 8, 1, 2, 4};
+		System.out.println(isAnagram("LISTEN", "silent"));
 	}
 
 	public static String duplicateChars(String str1, String str2) {
@@ -44,13 +45,100 @@ class Task1_10 {
 		String[] names = fullName.split(" ");
 
     StringBuilder result = new StringBuilder();
-		for (int i = 0; i < names.length; i++) {
-			if (i == 0) {
-					result.append(names[i]).append(" ");
-			} else {
-					result.append(Character.toUpperCase(names[i].charAt(0))).append(". ");
-			}
+		for (int i = 1; i < names.length; i++) {
+			result.append(Character.toUpperCase(names[i].charAt(0))).append(". ");
+		}
+		result.append(Character.toUpperCase(names[0].charAt(0)) + names[0].substring(1)).append(" ");
+		return result.toString().trim();
 	}
-	return result.toString().trim();
-}
+
+	public static double[] normalizator(double[] array) {
+    double min = Arrays.stream(array).min().getAsDouble();
+    double max = Arrays.stream(array).max().getAsDouble();
+
+    double[] normalizedArray = new double[array.length];
+
+    for (int i = 0; i < array.length; i++) {
+        normalizedArray[i] = (array[i] - min) / (max - min);
+    }
+
+    return normalizedArray;
+  } 
+
+  public static int[] compressedNums(double[] array) {
+    int[] result = new int[array.length];
+    int index = 0;
+
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] < 1.0) {
+            continue;
+        }
+        result[index++] = (int) Math.floor(array[i]);
+    }
+
+    result = Arrays.copyOf(result, index);
+
+    Arrays.sort(result);
+
+    return result;
+  }
+
+  public static String camelToSnake(String camelCase) {
+    String snakeCase = camelCase.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
+
+    return snakeCase;
+  }
+
+  public static int secondBiggest(int[] array) {
+    Arrays.sort(array);
+    return array[array.length - 2];
+  }
+
+  public static String localReverse(String input, char marker) {
+    StringBuilder result = new StringBuilder();
+    StringBuilder temp = new StringBuilder();
+    boolean insideMarkers = false;
+
+    for (char c : input.toCharArray()) {
+        if (c == marker) {
+            if (insideMarkers) {
+                // Если мы внутри маркерных символов, то развернем temp и добавим к результату
+                result.append(temp.reverse());
+                temp.setLength(0);
+            }
+            insideMarkers = !insideMarkers; 
+            result.append(marker);
+        } else if (insideMarkers) {
+            temp.append(c);
+        } else {
+            result.append(c);
+        }
+    }
+
+    return result.toString();
+  }
+
+  public static int equals(int num1, int num2, int num3) {
+    int count = 0;
+    if (num1 == num2) {
+      count++;
+    }
+    if (num1 == num3) {
+      count++;
+    }
+    if (num2 == num3) {
+      count++;
+    }
+    return count == 1 ? 2 : count;
+  }
+
+  public static boolean isAnagram(String str1, String str2) {
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    char[] charArray1 = str1.toCharArray();
+    char[] charArray2 = str2.toCharArray();
+    Arrays.sort(charArray1);
+    Arrays.sort(charArray2);
+    return Arrays.equals(charArray1, charArray2);
+  }
 }
